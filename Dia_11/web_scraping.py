6f7029244b0814f25b472ab3d8c544a3f0c3a1b6
Ivan Salcedo - Casa <ingivansalcedo@yahoo.com.co>
2023-07-16 22:33:57 -0500

    Extraer elementos de una clase

--- Dia_11/web_scraping.py
+++ Dia_11/web_scraping.py
@@ -1,14 +1,23 @@
 import bs4
 import requests
 
+'''Obtiene y consulta información de una página, el esqueleto'''
 resultado = requests.get('https://escueladirecta-blog.blogspot.com/2021/10/encapsulamiento-pilares-de-la.html')
 
 #print(resultado.text)
 
+''' La función lxml lo convierte en un formato más legible'''
 sopa = bs4.BeautifulSoup(resultado.text, 'lxml')
 
-#print(sopa.select('title')[0].getText())
-parrafoEspecial = sopa.select('p')
-print(parrafoEspecial)
+'''Ejemplo de como extraer informacion: Como ejemplo se utiliza la etiqueta <p></p>'''
+# for i in range(len(sopa.select('p'))):
+#     parrafoEspecial = sopa.select('p')[i].getText() #Obtiene solo el texto sin la configuración de la etiqueta
+#     print(parrafoEspecial)
+
+#header-image-wrapper
+claseEspecial = sopa.select('.post-body.entry-content.float-container p')
+for i in claseEspecial:
+    print(i.getText())
+
 
 
